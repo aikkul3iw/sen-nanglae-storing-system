593badd40e6cea61ed08f85d9eebded3e271ce6d
zack <zack@DESKTOP-CLHJ6CB>
2017-04-24 16:38:39 +0700

    User

---
+++ NanglaeStoreSystemEJB/ejbModule/com/proj/ejb/entity/User.java
@@ -0,0 +1,40 @@
+package com.proj.ejb.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long user_id;
+	
+	private String username;
+	private String password;
+	public long getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(long user_id) {
+		this.user_id = user_id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
---
+++ NanglaeStoreSystemSpring/src/com/mfu/proj/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.mfu.proj.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.proj.ejb.entity.User;
+import com.proj.ejb.face.UserService;
+
+
+	@Controller
+	public class UserController {
+
+		
+
+}
